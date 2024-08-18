9be2be3f13461b6c0d5a78747dedc0fbb6b28519
airliang <air_liang1212@163.com>
2024-08-18 16:42:04 +0800

    optimize ssao feature

--- Assets/Scripts/AutoMove.cs
+++ Assets/Scripts/AutoMove.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class AutoMove : MonoBehaviour
 {
     public float speed = 0.5f;
+    public float rotateSpeed = 10.0f;
     private float signDistance = 0;
     public Vector3 moveDirection = Vector3.right;
     public float changeDirectionDistance = 5;
     private Vector3 origPosition;
+    private float currentRotateDegree = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +21,13 @@ public class AutoMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        currentRotateDegree += rotateSpeed * Time.deltaTime;
+        //float test = 380.0f % 360.0f;
+        //if (currentRotateDegree > 360.0f)
+        {
+            currentRotateDegree %= 360.0f;
+        }
+        transform.localRotation = Quaternion.AngleAxis(currentRotateDegree, Vector3.up);
         bool bChangeDirection = Mathf.Abs(signDistance) > changeDirectionDistance;
         if (bChangeDirection)
             moveDirection = -moveDirection;
